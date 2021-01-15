d4878dc9b5b4cfb6cbac1ec889546b9cd3ba2892
haydensprinkle <haydensprinkle@gmail.com>
2021-01-14 22:32:28 -0800

    Linked current markup elements to JS variables

---
+++ app.js
@@ -0,0 +1,4 @@
+let timezoneEl = document.querySelector(".timezone");
+let iconEl = document.querySelector(".icon");
+let degreeEl = document.querySelector(".temperature-degree");
+let descriptionEl = document.querySelector(".temperature-description");
