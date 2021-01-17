09b50a7c1971f11a307957c18cc49376ad046d80
haydensprinkle <haydensprinkle@gmail.com>
2021-01-16 21:39:47 -0800

    Added functionality to grab current geolocation

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+window.addEventListener("load", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+    });
+  }
+});
+
 let timezoneEl = document.querySelector(".timezone");
 let iconEl = document.querySelector(".icon");
 let degreeEl = document.querySelector(".temperature-degree");
