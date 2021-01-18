8c8ab76b05aab85b9c89b7c7b461725b8dcb2cc8
haydensprinkle <haydensprinkle@gmail.com>
2021-01-17 21:59:08 -0800

    added API call and extracted data from response

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 window.addEventListener("load", () => {
+  let lon;
+  let lat;
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
+      lon = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const apiKey = "f066b7d9c3302e06523b896f42e1a5df";
+      const api = `${proxy}https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const { timezone } = data;
+          const { temp } = data.current;
+          const { description } = data.current.weather[0];
+          console.log(temp);
+          console.log(timezone);
+          console.log(description);
+        });
     });
   }
 });
